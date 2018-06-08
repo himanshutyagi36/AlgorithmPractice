15b416d85d52aff6336f59fc4a45fe0294f5fc87
Himanshu Tyagi <tyagi.himanshu90@gmail.com>
2018-06-08 13:00:43 -0400

    Update checkBST.py

--- CTCI/checkBST.py
+++ CTCI/checkBST.py
@@ -11,10 +11,13 @@ def checkBST(root):
     return (checkBSTUtil(root, INT_MIN, INT_MAX))
     
 def checkBSTUtil(root, min, max):
+    # if tree is empty
     if root is None:
         return True
+    # if it fails the value constraints
     if root.data < min or root.data > max:
         return False
+    # recursively check the left and right subtrees
     return (checkBSTUtil(root.left, min, root.data-1) and checkBSTUtil(root.right, root.data+1, max))
     
         
