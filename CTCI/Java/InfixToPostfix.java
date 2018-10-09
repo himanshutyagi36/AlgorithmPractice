7452cfd47d17c1e98280ae89f08684e61850c2d5
himanshutyagi36 <tyagi.himanshu90@gmail.com>
2018-10-08 22:20:40 -0400

    infixtopostfix

---
+++ CTCI/Java/InfixToPostfix.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class InfixToPostfix {
+    // A utility function to return precedence of a given operator 
+    // Higher returned value means higher precedence 
+    static int Prec(char ch) 
+    { 
+        switch (ch) 
+        { 
+        case '+': 
+        case '-': 
+            return 1; 
+       
+        case '*': 
+        case '/': 
+            return 2; 
+       
+        case '^': 
+            return 3; 
+        } 
+        return -1; 
+    } 
+    // conert infix expression to postfix
+    static String infixToPostfix(String s) {
+        if(s.length() == 0) return null;
+        String result = new String("");
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i); 
+            if(Character.isLetterOrDigit(c)) {
+                result+=c;
+            } else if(c=='(') {
+                stack.push(c);
+            } else if ( c== ')') {
+                while(!stack.isEmpty() && stack.peek() != '(')
+                    result+= stack.pop();
+                if (!stack.isEmpty() && stack.peek() != '(') 
+                return "Invalid Expression"; // invalid expression                 
+                else
+                    stack.pop(); 
+            } else {// an operator is encountered  
+                while (!stack.isEmpty() && Prec(c) <= Prec(stack.peek())) 
+                    result += stack.pop(); 
+                stack.push(c); 
+            } 
+        }
+        // pop all the operators from the stack 
+        while (!stack.isEmpty()) 
+            result += stack.pop(); 
+       
+        return result; 
+    }
+    public static void main(String[] args)  
+    { 
+        String exp = "a+b*(c^d-e)^(f+g*h)-i"; 
+        System.out.println(infixToPostfix(exp)); 
+    } 
+}
